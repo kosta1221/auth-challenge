5fbdce84758627efdd9f64d3e3692d01a3f11dc5
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-26 19:47:51 +0300

    setup basic authentication

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const app = express();
 
 morgan.token("reqbody", (req) => {
@@ -19,6 +20,41 @@ morgan.token("reqbody", (req) => {
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :reqbody"));
 
-app.post("/users/register", (req, res, next) => {});
+const users = [];
+
+app.get("/users", (req, res, next) => {
+	res.json(users);
+});
+
+app.post("/users/register", async (req, res, next) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+		console.log(hashedPassword);
+
+		const user = { name: req.body.name, password: hashedPassword };
+		users.push(user);
+		res.status(201).send();
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.post("/users/login", async (req, res, next) => {
+	console.log(req.body);
+	const user = users.find((user) => (user.name = req.body.name));
+	if (user == null) {
+		return res.status(400).send("Cannot find user");
+	}
+});
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error);
+	console.error(error.message);
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 module.exports = app;
